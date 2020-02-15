3b88e45dafc10516835f8ca8c576493cf229c37d
Martin Noren <herr.noren@gmail.com>
2020-02-15 13:59:58 +0100

    Add backend functionality to upload photo

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -1,7 +1,13 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using AutoMapper;
 using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
@@ -32,6 +38,54 @@ namespace DatingApp.API.Controllers
                 _cloudinaryConfig.Value.ApiSecret            
             );
             _cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, 
+            PhotoForCreationDto photoForCreationDto)
+        {
+            // check if user id in token is same as 'userId' to add photo to
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            
+            var userFromRepo = await _repo.GetUser(userId);
+
+            var file = photoForCreationDto.File;
+
+            var uploadResult = new ImageUploadResult();
+            if(file.Length > 0)
+            {
+                using(var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams()
+                    {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation()
+                            .Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);    // catch response from upload
+                }
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);  // map to type Photo
+            
+            if(!userFromRepo.Photos.Any(u => u.IsMain)) 
+            {
+                photo.IsMain = true;
+            }
+            
+            userFromRepo.Photos.Add(photo);
+            
+            if(await _repo.SaveAll())
+            {
+                return Ok();
+            }
+
+            return BadRequest("Could not add the photo!");
         }
 
         
