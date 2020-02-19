87a0f257cdfb537cc47fe6488e839da4fe69d35c
Martin Noren <herr.noren@gmail.com>
2020-02-19 21:10:52 +0100

    Add change main photo function in frontend

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -3,6 +3,8 @@ import { Photo } from 'src/app/_models/photo';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
 import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -15,7 +17,10 @@ export class PhotoEditorComponent implements OnInit {
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.initializeUploader();
@@ -54,4 +59,14 @@ export class PhotoEditorComponent implements OnInit {
       }
     };
   }
+
+  setMainPhoto(photo: Photo) {
+    this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id)
+      .subscribe(() => {
+        console.log('Successfully set to main');
+      }, error => {
+        this.alertify.error(error);
+      })
+  }
+
 }
